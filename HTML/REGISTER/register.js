e2a7946f93254f417220163f4b31db65b2b737f9
soledad <solzambrano.8@gmail.com>
2022-09-18 18:11:26 -0300

    instalacion

---
+++ HTML/REGISTER/register.js
@@ -0,0 +1,4 @@
+/**registro*/
+if(navigator.serviceWorker){
+    navigator.serviceWorker.register('./REGISTER/sw.js')//la ubicacion es respecto al html
+}
---
+++ HTML/REGISTER/sw.js
@@ -0,0 +1,41 @@
+/**creacion */
+const CACHE_ELEMENTS=[
+    './',
+     '../../js/formulario.js',
+     '../contact.html',
+     '../about.html',
+     '../gameInformation.html',
+     '../registration.html',
+     '../rules.html',
+     '../show_data.html',
+      '../../IMAGENES/soccer_image2.jpg',
+      '../../IMAGENES/soccer_image1.jpg',
+     '../../IMAGENES/soccer_image3.jpg',
+      '../../CSS/diseño_formulario.css',
+      '../../CSS/diseño.css'
+]
+/**si no se en live server abrir una nueva ventana */
+const CACHE_NAME='v1_cache';
+ self=this
+ self.addEventListener('install',(e)=>{
+    console.log(e)
+     e.waitUntil(caches.open(CACHE_NAME).then((cache)=>{
+        cache.addAll(CACHE_ELEMENTS).then(()=>{
+        console.log(cache)
+             self.skipWaiting()
+         })
+    .catch(console.log)
+     }))
+
+ })
+
+
+// /**activacion */
+// self.addEventListener("activate",(e)=>{
+//     const cacheWhiteList=[CACHE_NAME];
+//     e.waitUntil(caches.keys().then((cachesName)=>{
+//         return Promise.all(cachesName.map(cachesName=>{
+//             return cacheWhiteList.indexOf(cachesName)===-1 && caches.delete(cacheName)
+//         }))
+//     }))
+// })
